2e249fe1b224d18b3bc59a2d1c2e022a72ddd1b0
Beko <96618217+kaanbek0@users.noreply.github.com>
2024-03-31 14:32:48 +0000

    deneme denendi

--- Deneme.java
+++ Deneme.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
 public class Deneme
 {
+  public class Inner
+  {
+    private int balance;
+
+    public Inner(int a)
+    {
+      balance = a;
+    }
+
+    public int Balance()
+    {
+      return balance;
+    }
+
+    public void Deposit(int a)
+    {
+      balance += a;
+    }
+
+  }
   public static void main(String[] args)
   {
-    System.out.println("deneme");
+    
+    Scanner in = new Scanner(System.in);
+    int first = in.nextInt();
+    Inner acc = new Inner(first);
+    int second = in.nextInt();
+    acc.Deposit(second);
+
+    System.out.println(acc.Balance());
+    in.close();
   }
 }
 
