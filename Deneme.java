47c34b9d1febb94361862563f43a7fabe060eb69
Beko <96618217+kaanbek0@users.noreply.github.com>
2024-05-16 19:20:43 +0000

    Tested the vscode extention

--- Deneme.java
+++ Deneme.java
@@ -1,37 +1,9 @@
 import java.util.Scanner;
-public class Deneme
-{
-  public class Inner
-  {
-    private int balance;
-
-    public Inner(int a)
-    {
-      balance = a;
-    }
-
-    public int Balance()
-    {
-      return balance;
-    }
-
-    public void Deposit(int a)
-    {
-      balance += a;
-    }
-
-  }
+public class Deneme{
   public static void main(String[] args)
   {
+    System.out.println("deneme");
     
-    Scanner in = new Scanner(System.in);
-    int first = in.nextInt();
-    Inner acc = new Inner(first);
-    int second = in.nextInt();
-    acc.Deposit(second);
-
-    System.out.println(acc.Balance());
-    in.close();
   }
 }
 
